f29bb75efe9beff7b362171be5192c6cf98e4f23
kataynoi <thait-rex@hotmail.com>
2016-10-25 17:05:22 +0700

    first

---
+++ common/models/Person.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "person".
+ *
+ * @property integer $user_id
+ * @property string $firstname
+ * @property string $lastname
+ * @property string $photo
+ * @property string $address
+ * @property string $tel
+ * @property integer $department_id
+ * @property string $office_id
+ * @property string $email
+ * @property string $facebook
+ *
+ * @property User $user
+ */
+class Person extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'person';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'firstname', 'lastname', 'department_id', 'office_id'], 'required'],
+            [['user_id', 'department_id'], 'integer'],
+            [['address'], 'string'],
+            [['firstname', 'lastname', 'photo', 'email', 'facebook'], 'string', 'max' => 100],
+            [['tel'], 'string', 'max' => 45],
+            [['office_id'], 'string', 'max' => 5],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'user_id' => 'User ID',
+            'firstname' => 'ชื่อ',
+            'lastname' => 'นามสกุล',
+            'photo' => 'รูปภาพ',
+            'address' => 'ที่อยู่',
+            'tel' => 'เบอร์โทร',
+            'department_id' => 'ฝ่าย',
+            'office_id' => 'สถานบริการ',
+            'email' => 'อีเมลล์',
+            'facebook' => 'Facebook',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
